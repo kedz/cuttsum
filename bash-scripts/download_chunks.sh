66db813c324ba5a613db4366b98192052f9888a8
Chris Kedzie <kedzie@cs.columbia.edu>
2014-08-14 16:29:23 -0400

    Added srilm client python wrapper

--- bash-scripts/download_chunks.sh
+++
@@ -1,4 +0,0 @@
-DATA=/home/kedz/data/trec/
-RES=$DATA/resources
-python -u ../utility-scripts/download_chunks.py -c $RES/chunkurls.txt -e $RES/topics-masked.xml -w $DATA/event_chunks
-
--- bash-scripts/run_static_rel_extractor.sh
+++
@@ -1,6 +0,0 @@
-DATA=/home/kedz/data/trec
-RES=$DATA/resources
-OUT=/home/kedz/results/trec/relevant
-STATS=/home/kedz/results/trec/stats
-
-python -u python/static_relevance_pipeline.py -e $RES/training-topics.xml -c $DATA/training_chunks -t "2012 East Azerbaijan earthquakes" -o $OUT/2012_East_Azerbaijan_earthquakes.sc.xz -s $STATS/2012_East_Azerbaijan_earthquakes --n-threads 4
