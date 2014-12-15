cab8ad112d87abbc86876bf676e0e2fcfed090f1
Chris Kedzie <kedzie@cs.columbia.edu>
2014-12-15 16:58:29 -0500

    Cleaned up (gutted kba corpus), moved most of functionallity to resource management (cuttsum.data).

--- trec2014/python/cuttsum/kba.py
+++ trec2014/python/cuttsum/kba.py
@@ -1,93 +1,6 @@
 import streamcorpus as sc
-import gzip
-from datetime import datetime
-import urllib2
-from pkg_resources import resource_filename
-import os
-import errno
 
 class _KBAStreamCorpus(object):
-
-    def paths(self, start_dt=None, end_dt=None, domain_filter=None):
-
-        paths_fname = resource_filename(u'cuttsum', self.paths_txt_)
-        with gzip.open(paths_fname, u'r') as f:
-            for line in f:
-                path = line.strip()
-                dt_string, doc_string = path.split('/')
-                timestamp = datetime.strptime(dt_string, '%Y-%m-%d-%H')
-                domain, count = doc_string.split('-')[0:2]
-                if start_dt is not None and start_dt > timestamp:
-                    continue
-                if end_dt is not None and end_dt < timestamp:
-                    continue
-                if domain_filter is not None and domain not in domain_filter:
-                    continue
-                yield timestamp, domain, int(count), path
-
-    def chunk_paths(self, start_dt=None, end_dt=None, domain_filter=None,
-                    trec_dir=None):
-
-        if trec_dir is None:
-            trec_dir = os.getenv(u'TREC_DATA', u'.')
-
-        chunk_dir = os.path.join(trec_dir, self.fs_name())
-        chunk_dt_dirs = os.listdir(chunk_dir)
-        chunk_dt_dirs.sort()
-        for chunk_dt_dir in chunk_dt_dirs:
-                timestamp = datetime.strptime(chunk_dt_dir, '%Y-%m-%d-%H')
-                if start_dt is not None and start_dt > timestamp:
-                    continue
-                if end_dt is not None and end_dt < timestamp:
-                    continue
-
-                chunk_dt_path = os.path.join(chunk_dir, chunk_dt_dir)    
-                paths = []
-                for chunk in os.listdir(chunk_dt_path):
-                    dmn, count = chunk.split('-')[0:2]
-                    if domain_filter is not None and dmn not in domain_filter:
-                        continue
-                    paths.append(os.path.join(chunk_dt_path, chunk))
-                if len(paths) > 0:
-                    yield timestamp, paths
-
-    def download_path(self, path, dest_dir):
-        url = '{}{}'.format(self.aws_url_, path)
-        dest = os.path.join(dest_dir, path)
-        dest_parent = os.path.dirname(dest)
-        if not os.path.isdir(dest_parent):
-            try:
-                os.makedirs(dest_parent)
-            except OSError, e:
-                if e.errno == errno.EEXIST and os.path.isdir(dest_parent):
-                    # File exists, and it's a directory,
-                    # another process beat us to creating this dir, that's OK.
-                    pass
-                else:
-                    # Our target dir exists as a file, or different error,
-                    # reraise the error!
-                    raise
-        print url
-        u = urllib2.urlopen(url)
-        f = open(dest, 'wb')
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        #print "Downloading: %s Bytes: %s" % (path, file_size)
-
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            #status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            #status = status + chr(8)*(len(status)+1)
-            #print status,
-
-        f.close() 
 
     def year(self):
         return self.year_
@@ -114,10 +27,6 @@ documents that werer processed by LingPipe."""
         self.aws_url_ = u'http://s3.amazonaws.com/' + \
             u'aws-publicdatasets/trec/kba/' + \
             u'kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/'
-        self.paths_txt_ = \
-            os.path.join(u'2013-data',
-                u'kba-streamcorpus-2013-v0_2_0' + \
-                U'-english-and-unknown-language.s3-paths.txt.gz')
 
     def fs_name(self):
         return u'2013_english_and_unknown' 
@@ -138,9 +47,6 @@ documents that were processed by the BBN Serif tool."""
         self.aws_url_ = u'http://s3.amazonaws.com/' + \
             u'aws-publicdatasets/trec/kba/' + \
             u'kba-streamcorpus-2014-v0_3_0-serif-only/'
-        self.paths_txt_ = \
-            os.path.join(u'2014-data',
-                u'kba-streamcorpus-2014-v0_3_0-serif-only.s3-paths.txt.gz')
 
     def fs_name(self):
         return u'2014_serif_only' 
@@ -163,9 +69,6 @@ the TREC TS 2014 event queries."""
         self.aws_url_ = u'http://s3.amazonaws.com/' + \
             u'aws-publicdatasets/trec/ts/' + \
             u'streamcorpus-2014-v0_3_0-ts-filtered/'
-        self.paths_txt_ = \
-            os.path.join(u'2014-data',
-                u'streamcorpus-2014-v0_3_0-ts-filtered.s3-paths.txt.gz')
 
     def fs_name(self):
         return u'2014_filtered_ts' 
