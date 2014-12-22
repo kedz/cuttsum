c574c46dd98767587a607067737563d99be41db1
Chris Kedzie <kedzie@cs.columbia.edu>
2014-12-22 02:17:23 -0500

    Modified imports for cuttsum.pipeline

--- trec2014/python/cuttsum/pipeline/__init__.py
+++ trec2014/python/cuttsum/pipeline/__init__.py
@@ -1,6 +1,10 @@
-from representation import SalienceFeatureSet
+from representation import SalienceFeatureSet, TfIdfExtractor, \
+    LMProbExtractor
 import jobs
+from component import ArticlesResource, SentenceFeaturesResource
 
-__ALL__ = [u'SalienceFeatureSet', u'jobs']
+__ALL__ = [u'SalienceFeatureSet', u'TfIdfExtractor', u'LMProbExtractor', 
+           u'jobs', 
+           u'ArticlesResource', u'SentenceFeaturesResource']
 
 
