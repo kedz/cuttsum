6620e441fa8c1ea41e66ae333efec63ea13d5537
Chris Kedzie <kedzie@cs.columbia.edu>
2014-12-29 05:28:42 -0500

    Added minor changes to make-gigaword

--- trec2014/sbin/make-gigaword-lm.py
+++ trec2014/sbin/make-gigaword-lm.py
@@ -18,36 +18,45 @@ def main(**kwargs):
 
     max_files = len(paths)
 
-    pool = Pool(20)    
+    pool = Pool(5)    
 
+    
     with gzip.open(opath, u'w') as f:
 
-        pb = ProgressBar(max_files)
+        #pb = ProgressBar(max_files)
+        #for path in paths:
+        #    print path
+        #    result = worker(path)
         for result in pool.imap_unordered(worker, paths):
-            pb.update()
+            #pb.update()
             for sent_str in result:
-                print sent_str
                 s = sent_str.encode(u'utf-8')
                 f.write(s)
 
 def worker(path):
+ 
+    print path
     patt = re.compile(r'[_]+')
     sent_strings = []
-    for doc in corenlp.open_tar_gz(path):
-        for sent in doc:
-            buf = []
-            for token in sent:
-                if token.ne == u'O':
-                    words = patt.split(token.lem)
-                    for word in words:
-                        if word != u'':
-                            buf.append(word.lower())
-                else:
-                    buf.append(u'__{}__'.format(token.ne.lower()))
-                    
-            buf = u' '.join(buf)
-            buf = buf + u'\n'
-            sent_strings.append(buf)
+ 
+    try:
+        for doc in corenlp.open_tar_gz(path):
+            for sent in doc:
+                buf = []
+                for token in sent:
+                    if token.ne == u'O':
+                        words = patt.split(token.lem)
+                        for word in words:
+                            if word != u'':
+                                buf.append(word.lower())
+                    else:
+                        buf.append(u'__{}__'.format(token.ne.lower()))
+                        
+                buf = u' '.join(buf)
+                buf = buf + u'\n'
+                sent_strings.append(buf)
+    except IOError, e:
+        print e, path
     return sent_strings
 
 if __name__ == u'__main__':
