4c517d673d8303e2fe1721e3e62df888a24acaf2
Chris Kedzie <kedzie@cs.columbia.edu>
2014-12-29 00:01:09 -0500

    Added pipe aware progress bar

--- trec2014/python/cuttsum/misc.py
+++ trec2014/python/cuttsum/misc.py
@@ -6,7 +6,14 @@ class ProgressBar:
         self.n_job_ = 0
         self.term_width_ = 70
         self.bin_size_ = max_jobs / float(self.term_width_)
+        self.current_state_ = 0
     def update(self):
+        if sys.stdout.isatty():
+            self._update_term()
+        else:
+            self._update_log()
+
+    def _update_term(self):
         self.n_job_ += 1
         if self.n_job_ == self.max_jobs_:
             sys.stdout.write(' ' * 79 + '\r')
@@ -16,16 +23,35 @@ class ProgressBar:
             bins = min(bins, self.term_width_)
 
             if bins > 0:
-                bar = ' [' + '=' * (bins - 1) + '>' + \
+                bar = '\r [' + '=' * (bins - 1) + '>' + \
                     ' ' * (self.term_width_ - bins) + ']\r'
             else:
-                bar = ' [' + ' ' * self.term_width_ + ']\r'
+                bar = '\r [' + ' ' * self.term_width_ + ']\r'
 
             per = ' {:3.0f}% '.format(self.n_job_ * 100. / self.max_jobs_)
             bar = bar[:36] + per + bar[42:]
 
             sys.stdout.write(bar)
             sys.stdout.flush()
+
+    def _update_log(self):
+        self.n_job_ += 1
+        if self.n_job_ == self.max_jobs_:
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+        else:
+            per_complete = self.n_job_ * 100. / self.max_jobs_
+            state = round(per_complete / 5)
+            #print self.n_job_, state
+            if state != self.current_state_:
+                self.current_state_ = state
+                if state % 4 == 0:             
+                    sys.stdout.write('|')
+                else:
+                    sys.stdout.write('.')
+                sys.stdout.flush()
+
+
 
     def clear(self):
         sys.stdout.write('\r')
