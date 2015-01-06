75434c56ba7784bbfd29b17024f3b51ee77f007e
Chris Kedzie <kedzie@cs.columbia.edu>
2015-01-06 12:28:06 -0500

    Created multiprocess worker base class for resources and salience model

--- trec2014/python/cuttsum/data.py
+++ trec2014/python/cuttsum/data.py
@@ -48,8 +48,47 @@ def _init_resource_manager():
                 continue
             resources_[name] = clazz()
 
+class MultiProcessWorker(object):
 
-class Resource(object):
+    def do_work(self, worker, jobs, n_procs,
+                progress_bar, result_handler=None, **kwargs):
+        from .misc import ProgressBar
+        max_jobs = len(jobs)
+        job_queue = multiprocessing.Queue()
+        result_queue = multiprocessing.Queue()
+
+        for job in jobs:
+            job_queue.put(job)
+
+        pool = []        
+        for i in xrange(n_procs):
+            p = multiprocessing.Process(
+                target=worker, args=(job_queue, result_queue), kwargs=kwargs)
+            p.start()
+            pool.append(p)            
+
+            pb = ProgressBar(max_jobs)
+        try:
+            for n_job in xrange(max_jobs):
+                result = result_queue.get(block=True)
+                if result_handler is not None:
+                    result_handler(result)
+                if progress_bar is True:
+                    pb.update()
+
+            for p in pool:
+                p.join()
+
+        except KeyboardInterrupt:
+            pb.clear()
+            print "Completing current jobs and shutting down!"
+            while not job_queue.empty():
+                job_queue.get()
+            for p in pool:
+                p.join()
+            sys.exit()
+
+class Resource(MultiProcessWorker):
     def __init__(self):
         self.deps_met_ = set()
     
@@ -131,44 +170,5 @@ class Resource(object):
                 corpus = corpus.get_superset()
             return fun(self, event, corpus, **kwargs)
         return wrapper
-
-    def do_work(self, worker, jobs, n_procs,
-                progress_bar, result_handler=None, **kwargs):
-        from .misc import ProgressBar
-        max_jobs = len(jobs)
-        job_queue = multiprocessing.Queue()
-        result_queue = multiprocessing.Queue()
-
-        for job in jobs:
-            job_queue.put(job)
-
-        pool = []        
-        for i in xrange(n_procs):
-            p = multiprocessing.Process(
-                target=worker, args=(job_queue, result_queue), kwargs=kwargs)
-            p.start()
-            pool.append(p)            
-
-            pb = ProgressBar(max_jobs)
-        try:
-            for n_job in xrange(max_jobs):
-                result = result_queue.get(block=True)
-                if result_handler is not None:
-                    result_handler(result)
-                if progress_bar is True:
-                    pb.update()
-
-            for p in pool:
-                p.join()
-
-        except KeyboardInterrupt:
-            pb.clear()
-            print "Completing current jobs and shutting down!"
-            while not job_queue.empty():
-                job_queue.get()
-            for p in pool:
-                p.join()
-            sys.exit()
-
 
 
