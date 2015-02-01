90fdaa04e5426ecb3dcb2d4c1a539fd47bcc89a3
Chris Kedzie <kedzie@cs.columbia.edu>
2015-02-01 11:23:34 -0500

    Added amt judgements

--- trec2014/python/cuttsum/judgements.py
+++ trec2014/python/cuttsum/judgements.py
@@ -55,3 +55,14 @@ def get_2014_matches():
             names=[u'query id', u'update id', u'nugget id',
                    u'match start', u'match end', 'auto p'])
     return df
+
+def get_mturk_matches():
+    matches_tsv = resource_filename(
+            u'cuttsum', os.path.join(u'2015-data', u'mturk-matches.tsv.gz'))
+    with gzip.open(matches_tsv, u'r') as f:
+        df = pd.io.parsers.read_csv(
+            f, sep='\t', quoting=3, header=0,
+            dtype={u'match start': int, u'match end': int},
+            names=[u'query id', u'update id', u'nugget id',
+                   u'match start', u'match end', 'auto p'])
+    return df
