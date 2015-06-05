4c14b0e4b2d82b55245746624dc288e0af3672e4
Chris Kedzie <kedzie@cs.columbia.edu>
2015-06-05 11:48:49 -0400

    Oracle summarizers now handle soft matches.

--- trec2015/cuttsum/summarizers/_oracle.py
+++ trec2015/cuttsum/summarizers/_oracle.py
@@ -1,6 +1,7 @@
 import os
 from cuttsum.resources import MultiProcessWorker
 from cuttsum.pipeline import ArticlesResource
+from cuttsum.misc import si2df
 import cuttsum.judgements
 import numpy as np
 import pandas as pd
@@ -42,21 +43,22 @@ class RetrospectiveMonotoneSubmodularOracle(MultiProcessWorker):
             event, corpus, extractor, budget, soft_match)
 
         ## Set up summarizer ###
+
+        # This is the monotone submodular objective function (basically
+        # nugget coverage).
         def f_of_A(system, A, V_min_A, e, input_df, ndarray_data):
             return len(
                 set([nugget for nuggets in input_df.ix[A, "nuggets"].tolist()
                      for nugget in nuggets]))
 
-
         system = sumpy.system.MonotoneSubmodularBasic(f_of_A=f_of_A, k=budget)
 
+        # Get gold matchings for oracle.
         articles = ArticlesResource()
-            
         all_matches = cuttsum.judgements.get_merged_dataframe()
         matches = all_matches[all_matches["query id"] == event.query_id]
         
-        from cuttsum.misc import si2df
-
+        # Set up soft matching if we are using it.
         if soft_match is True:
             from cuttsum.classifiers import NuggetClassifier
             classify_nuggets = NuggetClassifier().get_classifier(event)
@@ -64,18 +66,27 @@ class RetrospectiveMonotoneSubmodularOracle(MultiProcessWorker):
                 judged = cuttsum.judgements.get_2013_updates() 
                 judged = judged[judged["query id"] == event.query_id]
                 judged_uids = set(judged["update id"].tolist())
+            else:
+                raise Exception("Bad corpus!")
   
+        # All sentences containing nuggets will go in all_df.
         all_df = [] 
         
+        # Pull out articles with nuggets.
         for hour, path, si in articles.streamitem_iter(
                 event, corpus, extractor):
 
+            # Convert stream item to dataframe and add gold label nuggets.
             df = si2df(si)
             df["nuggets"] = df["update id"].apply(
                 lambda x: set(
                     matches[matches["update id"] == x]["nugget id"].tolist()))
 
+            # Perform soft nugget matching on unjudged sentences.
             if soft_match is True:
+                ### NOTE BENE: geting an array of indices to index unjudged
+                # sentences so I can force pandas to return a view and not a
+                # copy.
                 I = np.where(
                     df["update id"].apply(lambda x: x not in judged_uids))[0]
                 
@@ -86,10 +97,12 @@ class RetrospectiveMonotoneSubmodularOracle(MultiProcessWorker):
 
                 df.loc[I, "nuggets"] = classify_nuggets(unjudged_sents)
             
-
+            # Add sentences with nuggets to final set for summarzing
             df = df[df["nuggets"].apply(len) > 0]
             all_df.append(df)
 
+        # Collect all dataframes into one and reset index (ALWAYS RESET 
+        # THE INDEX because pandas hates me.)
         all_df = pd.concat(all_df)
         all_df.reset_index(inplace=True)
 
@@ -97,8 +110,8 @@ class RetrospectiveMonotoneSubmodularOracle(MultiProcessWorker):
         F_of_S = len(
             set(n for ns in summary._df["nuggets"].tolist() for n in ns))
 
-        print "F(S)", F_of_S
-        print "summary nuggets" 
+        #print "F(S)", F_of_S
+        #print "summary nuggets" 
         sum_nuggets = list(set(
             n for ns in summary._df["nuggets"].tolist() for n in ns))
         sum_nuggets.sort()
@@ -117,13 +130,14 @@ class RetrospectiveMonotoneSubmodularOracle(MultiProcessWorker):
         output_df = pd.DataFrame(
             [{"Cum. F(S)": F_of_S, 
               "F(S)": F_of_S, 
-              "UB no const.": total_nuggets, 
+              "UB no const.": len(possible_nuggets), #      total_nuggets, 
               "budget": budget, 
               "Tot. Updates": len(summary._df), 
               "event title": event.fs_name(), 
               "timestamp": timestamp, 
               "query id": event.query_id},],
-            columns=["timestamp", "query id", "event title", "Cum. F(S)", "F(S)", "UB no const.",
+            columns=["timestamp", "query id", "event title", "Cum. F(S)", 
+                     "F(S)", "UB no const.",
                      "Tot. Updates", "budget",])   
         
         parent = os.path.dirname(output_path_prefix)
@@ -135,10 +149,11 @@ class RetrospectiveMonotoneSubmodularOracle(MultiProcessWorker):
         
         with open(stats_path, "w") as f:
             output_df.to_csv(f, sep="\t", index=False)
-        summary._df["sent text"] = summary._df["sent text"].apply(lambda x: x.encode("utf-8"))
+        summary._df["sent text"] = summary._df["sent text"].apply(
+            lambda x: x.encode("utf-8"))
         with open(updates_path, "w") as f:
-            summary._df[["timestamp", "update id", "sent text"]].sort(["update id"]).to_csv(
-                f, sep="\t", index=False)
+            summary._df[["timestamp", "update id", "sent text"]].sort(
+                ["update id"]).to_csv(f, sep="\t", index=False)
 
 
 class MonotoneSubmodularOracle(MultiProcessWorker):
@@ -170,43 +185,89 @@ class MonotoneSubmodularOracle(MultiProcessWorker):
         soft_match = kwargs.get("soft_match", False)
         budget = kwargs.get("budget", 25)
         
-        output_path_prefix = self.get_path_prefix(event, corpus, extractor, budget, soft_match)
+        output_path_prefix = self.get_path_prefix(
+            event, corpus, extractor, budget, soft_match)
        
         ## Set up summarizer ###
         def f_of_A(system, A, V_min_A, e, input_df, ndarray_data):
             return len(
-                set([nugget for nuggets in input_df.ix[A, "nuggets"].tolist() for nugget in nuggets]))
+                set([nugget for nuggets in input_df.ix[A, "nuggets"].tolist() 
+                     for nugget in nuggets]))
         system = sumpy.system.MonotoneSubmodularBasic(f_of_A=f_of_A, k=budget)
+
+        # Collect all previously collected nuggets here.
         nugget_cache = set()
 
-        articles = ArticlesResource()
-            
+        # Get gold matchings for oracle.
+        articles = ArticlesResource()            
         all_matches = cuttsum.judgements.get_merged_dataframe()
         matches = all_matches[all_matches["query id"] == event.query_id]
         
-        from cuttsum.misc import si2df
+        # Set up soft matching if we are using it.
+        if soft_match is True:
+            from cuttsum.classifiers import NuggetClassifier
+            classify_nuggets = NuggetClassifier().get_classifier(event)
+            if event.query_id.startswith("TS13"):
+                judged = cuttsum.judgements.get_2013_updates() 
+                judged = judged[judged["query id"] == event.query_id]
+                judged_uids = set(judged["update id"].tolist())
+            else:
+                raise Exception("Bad corpus!")
+ 
 
+        # Collect stats for each document here.
         stats = []
+        
+        # Aggregate summaries in summary_df.
         summary_df = []
         cum_F_of_S = 0
-        event_nuggets = set(matches["nugget id"].tolist())
-        total_nuggets = len(event_nuggets)
+
+        all_seen_nuggets = set()
+
+#        event_nuggets = set(matches["nugget id"].tolist())
+#        total_nuggets = len(event_nuggets)
         total_updates = 0
 
+        # Pull out articles with nuggets.
         for hour, path, si in articles.streamitem_iter(
                 event, corpus, extractor):
 
+            # Convert stream item to dataframe and add gold label nuggets.
             df = si2df(si)
             df["nuggets"] = df["update id"].apply(
-                lambda x: set(matches[matches["update id"] == x]["nugget id"].tolist()))
+                lambda x: set(
+                    matches[matches["update id"] == x]["nugget id"].tolist()))
+
+            # Perform soft nugget matching on unjudged sentences.
+            if soft_match is True:
+                ### NOTE BENE: geting an array of indices to index unjudged
+                # sentences so I can force pandas to return a view and not a
+                # copy.
+                I = np.where(
+                    df["update id"].apply(lambda x: x not in judged_uids))[0]
+                
+                unjudged = df[
+                    df["update id"].apply(lambda x: x not in judged_uids)]
+                unjudged_sents = unjudged["sent text"].tolist()
+                assert len(unjudged_sents) == I.shape[0]
+
+                df.loc[I, "nuggets"] = classify_nuggets(unjudged_sents)
             
+            # Remove nuggets from dataframe if we have already collected them in
+            # the cache. The scoring function should ignore these.
             df = df[df["nuggets"].apply(len) > 0]
-            df["nuggets"] = df["nuggets"].apply(lambda x: x.difference(nugget_cache))
+            all_seen_nuggets.update(
+                set(n for ns in df["nuggets"].tolist() for n in ns))
+            df["nuggets"] = df["nuggets"].apply(
+                lambda x: x.difference(nugget_cache))
             if len(df) == 0:
                 continue
-            summary =  system.summarize(df)
-            summary_nuggets = set(n for ns in summary._df["nuggets"].tolist() for n in ns)
-            nugget_cache.update(summary_nuggets)  
+
+            # Run summarizer on current document and update stats about it.
+            summary = system.summarize(df)
+            summary_nuggets = set(n for ns in summary._df["nuggets"].tolist() 
+                                  for n in ns)
+            nugget_cache.update(summary_nuggets)
             system.k -= len(summary._df)
 
             F_of_S = len(summary_nuggets)
@@ -217,7 +278,7 @@ class MonotoneSubmodularOracle(MultiProcessWorker):
             stats.append({
                 "Cum. F(S)": cum_F_of_S, 
                 "F(S)": F_of_S, 
-                "UB no const.": total_nuggets, 
+                "UB no const.": len(all_seen_nuggets), 
                 "budget": budget, 
                 "Tot. Updates": total_updates, 
                 "event title": event.fs_name(), 
@@ -231,23 +292,24 @@ class MonotoneSubmodularOracle(MultiProcessWorker):
 
 
         output_df = pd.DataFrame(stats,
-            columns=["timestamp", "query id", "event title", "Cum. F(S)", "F(S)", "UB no const.",
+            columns=["timestamp", "query id", "event title", 
+                     "Cum. F(S)", "F(S)", "UB no const.",
                      "Tot. Updates", "budget",])   
-        
+       
+        # Write stats and updates to file. 
         parent = os.path.dirname(output_path_prefix)
         if not os.path.exists(parent):
             os.makedirs(parent)
 
         stats_path = output_path_prefix + ".stats.tsv"
         updates_path = output_path_prefix + ".updates.tsv"
-        
 
         with open(stats_path, "w") as f:
             output_df.to_csv(f, sep="\t", index=False)
 
         summary_df = pd.concat(summary_df)
-        summary_df["sent text"] = summary_df["sent text"].apply(lambda x: x.encode("utf-8"))
+        summary_df["sent text"] = summary_df["sent text"].apply(
+            lambda x: x.encode("utf-8"))
         with open(updates_path, "w") as f:
-            summary_df[["timestamp", "update id", "sent text"]].sort(["update id"]).to_csv(
-                f, sep="\t", index=False)
-
+            summary_df[["timestamp", "update id", "sent text"]].sort(
+                ["update id"]).to_csv(f, sep="\t", index=False)
