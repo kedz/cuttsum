ce35454b504e708bed2de5f26f7bd05f4116f3da
Chris Kedzie <kedzie@cs.columbia.edu>
2015-06-18 14:41:31 -0400

    Exposed feature extractor and input stream to pipeline module.

--- trec2015/cuttsum/pipeline/__init__.py
+++ trec2015/cuttsum/pipeline/__init__.py
@@ -1,5 +1,7 @@
 from cuttsum.pipeline._article_annotator import ArticlesResource
 from cuttsum.pipeline._dedupe import DedupedArticlesResource
+from cuttsum.pipeline._features import SentenceFeaturesResource
 from cuttsum.pipeline._input_stream import InputStreamResource
 
-__all__ = ["ArticlesResource", "DedupedArticlesResource", "InputStreamResource"]
+__all__ = ["ArticlesResource", "DedupedArticlesResource", 
+           "SentenceFeaturesResource", "InputStreamResource"]
