60b64a0e8dc3f2707abee22a17012858fd09dbfd
Chris Kedzie <kedzie@cs.columbia.edu>
2015-06-19 19:19:56 -0400

    Added test events, and cleaned up report printing code. Reports are now generated for every training iteration.

--- trec2015/sbin/vwlearner.py
+++ trec2015/sbin/vwlearner.py
@@ -21,6 +21,7 @@ def score_sequence(actions, dataframes):
     d = 0
     sents = [i for i in xrange(len(dataframes[0]))]
     correct_decisions = 0
+    n_sents = 0
     for i, a in enumerate(actions, 1):
         if a < len(sents):
             df = dataframes[d].iloc[sents].reset_index(drop=True)
@@ -42,6 +43,9 @@ def score_sequence(actions, dataframes):
             ncache.update(update["nuggets"])
             update["rec"] = len(ncache) / float(len(all_nuggets))
             
+            n_sents += 1
+            update["avg. gain"] = len(ncache) / float(n_sents)
+
             data.append(update)                            
             del sents[a]
 
@@ -70,6 +74,7 @@ def score_sequence(actions, dataframes):
                 "action": "next",
                 "acc": correct_decisions / float(i),
                 "rec": len(ncache) / float(len(all_nuggets)),
+                "avg. gain": 0 if n_sents == 0 else len(ncache) / float(n_sents),
             }
             data.append(update)
 
@@ -78,7 +83,7 @@ def score_sequence(actions, dataframes):
                 sents = [i for i in xrange(len(dataframes[d]))]
 
     results_df = pd.DataFrame(data,
-        columns=["update id", "timestamp", "acc", "rec", "sent text", "action", "gain", 
+        columns=["update id", "timestamp", "acc", "rec", "avg. gain", "sent text", "action", "gain", 
                  "max gain", "num nuggets", "max nuggets", "nuggets"])
     return results_df
 
@@ -229,6 +234,82 @@ class LessPerfectOracle(pyvw.SearchTask):
         return output
 
 
+class LexicalFeaturesNextOracle(pyvw.SearchTask):
+    def __init__(self, vw, sch, num_actions):
+        pyvw.SearchTask.__init__(self, vw, sch, num_actions)
+        sch.set_options( sch.AUTO_HAMMING_LOSS | sch.IS_LDF ) # | sch.AUTO_CONDITION_FEATURES )
+
+    def make_example(self, sent, df, ncache):
+        tokens = df.iloc[sent]["lemmas stopped"]
+        ex = self.example(lambda:
+            {"a": [tok for tok in tokens] if len(tokens) > 0 else ["none"],
+             "b": [tok for tok in tokens if tok in ncache],
+            },
+            labelType=self.vw.lCostSensitive)
+        return ex
+
+    def _run(self, (event, docs)):
+
+        ncache = set()
+        output = []
+        n = 0
+
+        for doc in docs:
+
+            sents = range(len(doc))
+
+            while 1:
+
+                n += 1
+                # Create some examples, one for each sentence.
+                examples = [self.make_example(sent, doc, ncache) for sent in sents]
+                
+
+                # Create a final example for the option "next document".
+                # This example has the feature "all_clear" if the max gain
+                # of adding any current sentence to the summary is 0.
+                # Otherwise the feature "stay" is active.
+                gain = doc.iloc[sents]["nuggets"].apply(
+                    lambda x: len(x.difference(ncache))).values
+
+                examples.append(
+                    self.example(lambda:
+                        {"c": ["all_clear" if np.sum(gain) == 0 else "stay"],},
+                        labelType=self.vw.lCostSensitive))
+               
+                # Compute oracle. If max gain > 0, oracle always picks the 
+                # sentence with the max gain. Else, oracle picks the last 
+                # example which is the "next document" option.
+                if len(sents) > 0:
+                    oracle = np.argmax(gain) 
+                    oracle_gain = gain[oracle] 
+                    if oracle_gain == 0:
+                        oracle = len(sents)
+                else:
+                    oracle = 0
+                    oracle_gain = 0
+
+                # Make prediction.
+                pred = self.sch.predict(
+                    examples=examples,
+                    my_tag=n,
+                    oracle=oracle,
+                    condition=[], # (n-1, "p"), ])
+                )
+                output.append(pred)
+                
+                if pred < len(sents):
+                    ncache.update(doc.iloc[pred]["lemmas stopped"])
+                    del sents[pred]
+                                        
+                elif pred == len(sents):
+                    break
+   
+        return output
+
+
+
+
 
 class UpdateSummarizer(pyvw.SearchTask):
     def __init__(self, vw, sch, num_actions):
@@ -340,7 +421,7 @@ class UpdateSummarizer(pyvw.SearchTask):
         return output
 
 
-def main(learner, training_ids, n_iters, report_dir):
+def main(learner, training_ids, test_ids, n_iters, report_dir_base):
 
     extractor = "goose" 
     topk = 20
@@ -349,8 +430,9 @@ def main(learner, training_ids, n_iters, report_dir):
     res = InputStreamResource()
 
     events = [e for e in cuttsum.events.get_events()
-              if e.query_num in training_ids]
+              if e.query_num in training_ids or e.query_num in test_ids]
     training_insts = []
+    test_insts = []
     for event in events:
         print "Loading event", event.fs_name()
         corpus = cuttsum.corpora.get_raw_corpus(event)
@@ -359,18 +441,21 @@ def main(learner, training_ids, n_iters, report_dir):
         # This is the events document stream.
         dataframes = res.get_dataframes(event, corpus, extractor, threshold,
                 delay, topk)
-        import random
-        arange = [x for x in xrange(len(dataframes))]
-        random.shuffle(arange)
-        arange = arange[:75]
-        arange.sort()
-        print arange
-        dataframes = [dataframes[i] for i in arange]
-        #dataframes = dataframes[:10]
-        training_insts.append((event, dataframes))
 
+        if event.query_num in training_ids:
+            import random
+            arange = [x for x in xrange(len(dataframes))]
+            random.shuffle(arange)
+            arange = arange[:75]
+            arange.sort()
+            print arange
+            dataframes = [dataframes[i] for i in arange]
+            #dataframes = dataframes[:10]
+            training_insts.append((event, dataframes))
+            
+        if event.query_num in test_ids:
+            test_insts.append((event, dataframes))    
 
-        
     # Init l2s task.
     vw = pyvw.vw("--search 0 --csoaa_ldf m --search_task hook --ring_size 1024  --quiet  --search_no_caching")
 
@@ -379,38 +464,58 @@ def main(learner, training_ids, n_iters, report_dir):
         task = vw.init_search_task(PerfectOracle)
     elif learner == "LessPerfectOracle":
         task = vw.init_search_task(LessPerfectOracle)
+    elif learner == "LexicalFeaturesNextOracle":
+        task = vw.init_search_task(LessPerfectOracle)
     
 
-    for x in range(n_iters):
-        print "iter", x + 1
+    for n_iter in range(n_iters):
+        print "iter", n_iter + 1
         task.learn(training_insts)
 
-    for event, dataframes in training_insts:
-        # Predict a sequence for this training examples and see if it is sensible.
-        print "PREDICTING", event.fs_name()
-        sequence = task.predict((event, dataframes))
-        print sequence
+        for event, dataframes in training_insts:
+            # Predict a sequence for this training examples and see if it is sensible.
+            print "PREDICTING", event.fs_name()
+            sequence = task.predict((event, dataframes))
+            print sequence
+            make_report(event, dataframes, sequence, "train", n_iter, report_dir_base)
 
-        # Run through the sequence of decisions.
-        df = score_sequence(sequence, dataframes)
-        print df[df.columns[1:-1]]
-        results_path = os.path.join(report_dir, event.fs_name() + ".tsv")
-        with open(results_path, "w") as f:
-            df.to_csv(f, index=False, sep="\t")
-        df["timestamp"] = df["timestamp"].apply(datetime.utcfromtimestamp)
-        df.set_index("timestamp")[["acc", "rec"]].plot()
-        plt.gcf().suptitle(event.title+ " " + learner)
-        plt.gcf().savefig(os.path.join(report_dir, "{}.png".format(event)))
+        for event, dataframes in test_insts:
+            # Predict a sequence for this training examples and see if it is sensible.
+            print "PREDICTING", event.fs_name()
+            sequence = task.predict((event, dataframes))
+            print sequence
+            make_report(event, dataframes, sequence, "test", n_iter, report_dir_base)
+
+def make_report(event, dataframes, sequence, part, n_iter, report_dir_base):
+
+    # Run through the sequence of decisions.
+    df = score_sequence(sequence, dataframes)
+    print df[df.columns[1:-1]]
+                
+    report_dir = os.path.join(
+        report_dir_base, "iter-{}".format(n_iter + 1), part)
+    if not os.path.exists(report_dir): os.makedirs(report_dir)
+
+    results_path = os.path.join(report_dir, event.fs_name() + ".tsv")
+    with open(results_path, "w") as f:
+        df.to_csv(f, index=False, sep="\t")
+    df["timestamp"] = df["timestamp"].apply(datetime.utcfromtimestamp)
+    df.set_index("timestamp")[["acc", "rec", "avg. gain"]].plot()
+    plt.gcf().suptitle(event.title+ " " + learner + " iter-{}".format(n_iter + 1))
+    plt.gcf().savefig(os.path.join(report_dir, "{}.png".format(event.fs_name())))
+
 if __name__ == "__main__":
 
     import argparse
     import os
     parser = argparse.ArgumentParser()
     parser.add_argument(u"--learner", type=unicode, choices=[
-        u"PerfectOracle", u"LessPerfectOracle", u"Lexical"],
+        u"PerfectOracle", u"LessPerfectOracle", u"LexicalFeaturesNextOracle"],
         help=u"Learner to run.")
 
     parser.add_argument(u"--training-event-ids", type=int, nargs=u"+",
+                        help=u"event ids to select.")
+    parser.add_argument(u"--test-event-ids", type=int, nargs=u"+",
                         help=u"event ids to select.")
     parser.add_argument(u"--report-dir",
                         help=u"Where to right outputs.")
@@ -424,8 +529,9 @@ if __name__ == "__main__":
     if not os.path.exists(report_dir):
         os.makedirs(report_dir)
     training_ids = args.training_event_ids    
+    test_ids = args.test_event_ids
     learner = args.learner
 
-    main(learner, training_ids, n_iters, report_dir)
+    main(learner, training_ids, test_ids, n_iters, report_dir)
 
 
