ca0be895b2cf7973bc24ba6f4f86cd9c5bf29bcb
Chris Kedzie <kedzie@cs.columbia.edu>
2015-06-19 17:09:06 -0400

    FeatureExtractor extracting pos lemmas, tokens, nes, etc.

--- trec2015/cuttsum/pipeline/_features.py
+++ trec2015/cuttsum/pipeline/_features.py
@@ -1,6 +1,8 @@
 from cuttsum.resources import MultiProcessWorker
 from cuttsum.pipeline import DedupedArticlesResource
+from cuttsum.misc import english_stopwords
 import cuttsum.srilm
+import gzip
 import corenlp as cnlp
 import os
 import re
@@ -17,14 +19,20 @@ class SentenceFeaturesResource(MultiProcessWorker):
         if not os.path.exists(self.dir_):
             os.makedirs(self.dir_)
 
+    def requires_services(self, event, corpus, **kwargs):
+        return ["corenlp"]    
+
     def get_path(self, event, corpus, extractor, threshold):
-        return os.path.join(
+        return os.path.join(self.dir_,
             corpus.fs_name(),
             extractor,
             str(threshold),
             event.fs_name() + ".tsv.gz")
 
-
+    def get_dataframe(self, event, corpus, extractor, threshold):
+        path = self.get_path(event, corpus, extractor, threshold)
+        with gzip.open(path, "r") as f:
+            return pd.read_csv(f, sep="\t", engine="python")
 
 
     def get_job_units(self, event, corpus, **kwargs):
@@ -37,6 +45,8 @@ class SentenceFeaturesResource(MultiProcessWorker):
             event, corpus, extractor, thresh)
         if not os.path.exists(path):
             return [0]
+        else:
+            return []
 
     def do_job_unit(self, event, corpus, unit, **kwargs):
         if unit != 0:
@@ -49,7 +59,7 @@ class SentenceFeaturesResource(MultiProcessWorker):
         dfiter = res.dataframe_iter(
             event, corpus, extractor, include_matches=None, 
             threshold=thresh)
-        lm = cuttsum.srilm.Client(9999, 3, True)
+#        lm = cuttsum.srilm.Client(9999, 3, True)
         cnlp_client = cnlp.client.CoreNLPClient()
 
         def heal_text(sent_text):
@@ -101,7 +111,7 @@ class SentenceFeaturesResource(MultiProcessWorker):
                 r"", sent_text)
             sent_text = sent_text.replace("\n", " ")
 
-            return sent_text
+            return sent_text.encode("utf-8")
 
         def get_number_feats(sent):
             feats = []
@@ -127,30 +137,68 @@ class SentenceFeaturesResource(MultiProcessWorker):
 
 
         from collections import defaultdict
+        stopwords = english_stopwords()
         mention_counts = defaultdict(int)
         total_mentions = 0
-        for df in dfiter:
-            df["lm"] = df["sent text"].apply(lambda x: lm.sentence_log_prob(x.encode("utf-8"))[1])
-            df["pretty text"] = df["sent text"].apply(heal_text)
-            df = df[df["pretty text"].apply(lambda x: len(x.strip())) > 0]
-            df = df.reset_index(drop=True)
-            for t in df["pretty text"].tolist():
-                print t
-            doc_text = u"\n".join(df["pretty text"].tolist())
-            doc = cnlp_client.annotate(doc_text)
-            doc_persons = set()
-            for sent in doc:
-                for ne, ne_type, span in sent.get_ne_spans():
-                    if ne_type == u"PERSON":
-                        mention_counts[ne] += 1
-                        total_mentions += 1
-                        doc_persons.add(ne)
-            dep_feats = map(get_number_feats, doc)
-            print dep_feats
-            for per in doc_persons:
-                print per, mention_counts[per], mention_counts[per] / float(total_mentions)
-            assert len(doc) == len(df)
-            print "---"
+
+
+        path = self.get_path(
+            event, corpus, extractor, thresh)
+        dirname = os.path.dirname(path)
+        if not os.path.exists(dirname): os.makedirs(dirname)
+
+        cols = ["update id", "stream id", "sent id", "timestamp", 
+                "pretty text", "tokens", "lemmas", "pos", "ne", 
+                "tokens stopped", "lemmas stopped"]
+        with gzip.open(path, "w") as f:
+            f.write("\t".join(cols) + "\n")
+            for df in dfiter:
+                #df["lm"] = df["sent text"].apply(lambda x: lm.sentence_log_prob(x.encode("utf-8"))[1])
+                df["pretty text"] = df["sent text"].apply(heal_text)
+                df = df[df["pretty text"].apply(lambda x: len(x.strip())) > 0]
+                df = df[df["pretty text"].apply(lambda x: len(x.split(" "))) < 200]
+                df = df.reset_index(drop=True)
+                if len(df) == 0:
+                    print "skipping"
+                    continue
+                doc_text = "\n".join(df["pretty text"].tolist())
+                
+                doc = cnlp_client.annotate(doc_text)
+                df["tokens"] = map(lambda sent: [str(tok) for tok in sent],
+                                   doc)
+                df["lemmas"] = map(lambda sent: [tok.lem.encode("utf-8") 
+                                                 for tok in sent],
+                                   doc)
+                df["pos"] = map(lambda sent: [tok.pos for tok in sent],
+                                doc)
+                
+                df["ne"] = map(lambda sent: [tok.ne for tok in sent],
+                               doc)
+                
+                df["tokens stopped"] = map(
+                    lambda sent: [str(tok) for tok in sent
+                                  if unicode(tok).lower() not in stopwords],
+                    doc)
+                df["lemmas stopped"] = map(
+                    lambda sent: [tok.lem.lower().encode("utf-8") for tok in sent
+                                  if unicode(tok).lower() not in stopwords],
+                    doc)
+                cols = ["update id", "stream id", "sent id", "timestamp", "pretty text", "tokens", "lemmas", "pos", "ne", "tokens stopped", "lemmas stopped"]
+                df[cols].to_csv(f, index=False, header=False, sep="\t")
+
+#            doc_persons = set()
+#            for sent in doc:
+#                for ne, ne_type, span in sent.get_ne_spans():
+#                    if ne_type == u"PERSON":
+#                        mention_counts[ne] += 1
+#                        total_mentions += 1
+#                        doc_persons.add(ne)
+#            dep_feats = map(get_number_feats, doc)
+#            print dep_feats
+#            for per in doc_persons:
+#                print per, mention_counts[per], mention_counts[per] / float(total_mentions)
+#            assert len(doc) == len(df)
+#            print "---"
             #print df[["lm", "sent text"]]
             #print 
 
