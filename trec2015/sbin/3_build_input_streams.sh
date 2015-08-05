35f78c1e6ecbbc9fe2832a0445c4d8438d618355
Chris Kedzie <kedzie@cs.columbia.edu>
2015-08-05 18:02:04 -0400

    Runner for regressor training.

--- trec2015/sbin/3_build_input_streams.sh
+++ trec2015/sbin/3_build_input_streams.sh
@@ -1,4 +1,20 @@
-~/openmpi/bin/mpirun -n 5 python -u job_manager.py --cmd start --n-procs 5 \
-    --event-ids 1 --resource-paths "cuttsum.pipeline.SentenceFeaturesResource" \
+#~/openmpi/bin/mpirun -n 10 python -u job_manager.py --cmd start --n-procs 10 \
+#    --event-ids 1 2 3 4 5 6 8 9 10 \
+#    --resource-paths "cuttsum.pipeline.SentenceFeaturesResource" \
+#    --config job-configs.ini --service-config service-config.ini
+
+#~/openmpi/bin/mpirun -n 40 python -u job_manager.py --cmd start --n-procs 40 \
+#    --event-ids 1 2 3 4 5 6 8 9 10 \
+#    --resource-paths "cuttsum.classifiers.NuggetClassifier" \
+#    --config job-configs.ini --service-config service-config.ini
+
+#~/openmpi/bin/mpirun -n 10 python -u job_manager.py --cmd start --n-procs 10 \
+#    --event-ids 1 2 3 4 5 6 8 9 10  \
+#    --resource-paths "cuttsum.pipeline.InputStreamResource" \
+#    --config job-configs.ini --service-config service-config.ini
+
+~/openmpi/bin/mpirun -n 10 python -u job_manager.py --cmd start --n-procs 10 \
+    --event-ids 1 2 3 4 5 6 8 9 10 \
+    --resource-paths "cuttsum.classifiers.NuggetRegressor" \
     --config job-configs.ini --service-config service-config.ini
-#~/openmpi/bin/mpirun -n 5 python -u job_manager.py --cmd start --n-procs 5 --event-ids 1 2 3 4 5 8 9 10 --resource-paths "cuttsum.pipeline.InputStreamResource" --config job-configs.ini
+
