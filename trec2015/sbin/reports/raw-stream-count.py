58fb3cc01123cd9a6ec4926248721369fedde75f
Chris Kedzie <kedzie@cs.columbia.edu>
2015-09-02 13:16:07 -0400

    added support for 2015 events

--- trec2015/sbin/reports/raw-stream-count.py
+++ trec2015/sbin/reports/raw-stream-count.py
@@ -1,3 +1,4 @@
+import streamcorpus as sc
 import cuttsum.events
 import cuttsum.corpora
 from cuttsum.trecdata import SCChunkResource
@@ -8,7 +9,7 @@ from datetime import datetime
 from collections import defaultdict
 import matplotlib.pylab as plt
 plt.style.use('ggplot')
-
+pd.set_option('display.max_rows', 500)
 
 pd.set_option('display.width', 200)
 
@@ -27,6 +28,9 @@ chunk_res = SCChunkResource()
 articles_res = ArticlesResource()
 ded_articles_res = DedupedArticlesResource()
 data = []
+
+event2ids = defaultdict(set)
+fltr_event2ids = defaultdict(set)
 for event in cuttsum.events.get_events():
     
     corpus = cuttsum.corpora.get_raw_corpus(event)
@@ -35,7 +39,14 @@ for event in cuttsum.events.get_events():
     hour2ded = defaultdict(int)
     hour2ded_fltr = defaultdict(int)
     ded_df = ded_articles_res.get_stats_df(event, corpus, "goose", .8)
+    
     if ded_df is not None:  
+
+        if event.query_num > 25:
+            for ids in ded_df["stream ids"].apply(eval).tolist():
+                for id1 in ids:
+                    event2ids[event.fs_name()].add(id1)
+
         for _, row in ded_df.iterrows():
             dt = datetime.utcfromtimestamp(row["earliest"])
             hour = datetime(dt.year, dt.month, dt.day, dt.hour)
@@ -46,10 +57,10 @@ for event in cuttsum.events.get_events():
     hour2goose = defaultdict(int)
 
     for hour in hours:
-        path = articles_res.get_chunk_path(event, "goose", hour)
+        path = articles_res.get_chunk_path(event, "goose", hour, corpus)
         if path is None:
             continue
-        print path
+        #print path
         fname = os.path.split(path)[1]
         num_goose = int(fname.split("-")[0])
         hour2goose[hour] = num_goose
@@ -62,7 +73,7 @@ for event in cuttsum.events.get_events():
                        
  
     for hour in hours:
-        raw_chunks = chunk_res.get_chunks_for_hour(hour, corpus)
+        raw_chunks = chunk_res.get_chunks_for_hour(hour, corpus, event)
         num_raw_si = 0
         
         for chunk in raw_chunks:
@@ -80,6 +91,73 @@ for event in cuttsum.events.get_events():
         })
 
 
+for event in cuttsum.events.get_events():
+    if event.query_num < 26: continue    
+    
+    corpus = cuttsum.corpora.FilteredTS2015()
+    hours = event.list_event_hours()
+
+    hour2ded = defaultdict(int)
+    hour2ded_fltr = defaultdict(int)
+    ded_df = ded_articles_res.get_stats_df(event, corpus, "goose", .8)
+
+
+    if ded_df is not None:  
+        for ids in ded_df["stream ids"].apply(eval).tolist():
+            for id1 in ids:
+                fltr_event2ids[event.fs_name()].add(id1)
+
+
+
+        for _, row in ded_df.iterrows():
+            dt = datetime.utcfromtimestamp(row["earliest"])
+            hour = datetime(dt.year, dt.month, dt.day, dt.hour)
+            hour2ded[hour] += 1
+            if row["match"] == True:
+                hour2ded_fltr[hour] += 1
+
+    hour2goose = defaultdict(int)
+
+    for hour in hours:
+        path = articles_res.get_chunk_path(event, "goose", hour, corpus)
+        if path is None:
+            continue
+        print path
+        fname = os.path.split(path)[1]
+        num_goose = int(fname.split("-")[0])
+        hour2goose[hour] = num_goose
+#    goose_df = articles_res.get_stats_df(event, "goose")
+#    if goose_df is not None:
+#        for _, row in goose_df.iterrows():
+#            dt = datetime.utcfromtimestamp(row["hour"])
+#            hour = datetime(dt.year, dt.month, dt.day, dt.hour)
+#            hour2goose[hour] = row["goose articles"]
+                       
+ 
+    for hour in hours:
+        print hour
+        raw_chunks = chunk_res.get_chunks_for_hour(hour, corpus, event)
+        num_raw_si = 0
+        
+        for chunk in raw_chunks:
+            fname = os.path.split(chunk)[1]
+            #num_raw_si += int(fname.split("-")[1])
+            with sc.Chunk(path=chunk, mode="rb", message=corpus.sc_msg()) as c:
+                for si in c:
+                    num_raw_si += 1
+        #num_fltr_si = len(articles_res.get_si(event, corpus, "goose", hour))
+        data.append({
+            "event": event.query_id + " (filtered)",
+            "title": event.title,
+            "hour": hour,
+            "raw articles": num_raw_si,
+            "goose articles": hour2goose[hour],
+            "deduped articles": hour2ded[hour],
+            "deduped match articles": hour2ded_fltr[hour],
+        })
+
+
+
 df = pd.DataFrame(data)
 cols = ["raw articles", "goose articles", "deduped articles", 
         "deduped match articles"]
@@ -94,6 +172,31 @@ df_sum["deduped match articles"] = df_sum["deduped match articles"].apply(format
 
 
 print df_sum
+print
+
+
+coverage = []
+for event in cuttsum.events.get_events():
+    if event.query_num < 26: continue 
+
+    isect = event2ids[event.fs_name()].intersection(fltr_event2ids[event.fs_name()])
+    n_isect = len(isect)
+    n_unfltr = max(len(event2ids[event.fs_name()]), 1)
+    n_fltr = max(len(fltr_event2ids[event.fs_name()]), 1)
+    print event.fs_name()
+    print n_isect, float(n_isect) / n_fltr, float(n_isect) / n_unfltr
+    coverage.append({
+        "event": event.query_id,
+        "intersection": n_isect,
+        "isect/n_2015F": float(n_isect) / n_fltr,
+        "isect/n_2014": float(n_isect) / n_unfltr,
+    })
+
+df = pd.DataFrame(coverage)
+df_u = df.mean()
+df_u["event"] = "mean"
+print pd.concat([df, df_u.to_frame().T]).set_index("event")
+
 exit()
 with open("article_count.tex", "w") as f:
     f.write(df_sum.to_latex())
@@ -128,3 +231,5 @@ for (event, title), group in df.groupby(["event", "title"]):
     plt.gcf().suptitle(title)
     plt.gcf().savefig(os.path.join("plots", "{}-stream.png".format(event)))
     plt.close("all")
+
+
